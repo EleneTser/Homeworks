a4e5f2ca128a31cb6795494f028ddaf8bf551d09
Elene Cercvadze <elenecercvadze404@gmail.com>
2026-05-13 18:51:54 +0400

    new

--- HW3.js
+++ HW3.js
@@ -10,23 +10,15 @@
 
 //3) დააბრუნე ყველა დადებითი რიცხვის ჯამი
 // let arr = [-4, 2,-6,3,3,7,-8]
-// let sumofpositives = 0
-// for(let i = 0;i <arr.length; i++){
-//     if(arr[i]>0){
-//         sumofpositives += arr[i]
-//     }
-// }
+// let filteredarrr = arr.filter((num) => num > 0).reduce((tot,curr) => tot + curr,0)
+// console.log(filteredarrr)
 
 // console.log(sumofpositives)
 
 //4) მოცემული სტრინგების მასივიდან წაშალე თითოეული სტრინგის ბოლო სიმბოლო
-// let namesArr = ["giorgi","nika","mariami"]
-// let result = []
-// for(let i = 0; i< namesArr.length; i++){
-//     result.push(namesArr[i].slice(0, -1))
-// }
-
-// console.log(result)
+let namesArr = ["giorgi","nika","mariami"]
+let mappedarr = namesArr.map((num) => num.slice(0,-1))
+console.log(mappedarr)
 
 
 //5) გაამრავლე ყველა ელემენტი მასივში 2-ზე და შემდეგ ამოიღე რიცხვები, რომლებიც იყოფა 3-ზე
@@ -52,6 +44,6 @@
 // console.log(unque)
 
 //9) დააბრუნეთ ორი ყველაზე მცირე რიცხვის ჯამს 
-let arr = [-1,20,90,4,5,111]
-let sortedarr = arr.sort((a,b) => a-b)
-console.log(sortedarr[0] + sortedarr[1])
+// let arr = [-1,20,90,4,5,111]
+// let sortedarr = arr.sort((a,b) => a-b)
+// console.log(sortedarr[0] + sortedarr[1])
